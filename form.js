f541ced4c1d3a9bd4b0d818b2f3667d4d1636b60
Maria <mia.gotcha@proton.me>
2026-03-27 21:10:16 +0100

    add icons on profile page

--- form.js
+++ form.js
@@ -1,4 +1,5 @@
-/* characters left - my question 
+/* characters left - my question */
+
 const form = document.querySelector('[data-js="form"]');
 const messageInput = document.querySelector('[data-js="new-question-input"]');
 const charactersLeftElement = document.querySelector(
@@ -8,27 +9,30 @@ const charactersLeftElement = document.querySelector(
 messageInput.addEventListener("input", (event) => {
   charactersLeftElement.textContent = 150 - messageInput.value.length;
 });
-*/
 
 /* submit button */
-const form = document.querySelector('[data-js="form"]');
 
 const newQuestionCard = document.createElement("article");
+const newBookmark = document.createElement("img");
 const newQuestion = document.createElement("h3");
 const newAnswer = document.createElement("p");
+const newAnswerButton = document.createElement("button");
 const newList = document.createElement("ul");
 const newTag = document.createElement("li");
 
 newQuestionCard.classList.add("question-card");
+newBookmark.classList.add("question-card__bookmark");
 newQuestion.classList.add("question-card__question");
 newAnswer.classList.add("question-card__answer");
+newAnswerButton.classList.add("answer-button");
 newList.classList.add("category-list");
-newTag.classList.add("input");
+newTag.classList.add("category");
 
 newQuestion.textContent =
   "Which 1975 song by Queen became a worldwide hit and is considered their breakthrough single?";
 newAnswer.textContent = "Bohemian Rhapsody";
 newTag.textContent = "Queen";
+newAnswerButton.textContent = "Show answer";
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -36,9 +40,24 @@ form.addEventListener("submit", (event) => {
   const formData = new FormData(formElement);
   const data = Object.fromEntries(formData);
   console.log(data);
-  newQuestionCard.append(newImg, newQuestion, newAnswer, newList);
+  newQuestionCard.append(
+    newBookmark,
+    newQuestion,
+    newAnswer,
+    newAnswerButton,
+    newList,
+  );
   newList.append(newTag);
   document.body.append(newQuestionCard);
   formElement.reset();
   formElement.elements.newQuestion.focus();
 });
+
+/* solution der social medie post challlenge - hilfreich für bonusaufgabe?
+
+function handleLikeButtonClick(event) {
+  const buttonElement = event.target;
+  buttonElement.classList.toggle("post__button--liked");
+}
+newAnswerButton.addEventListener("click", handleLikeButtonClick);
+*/
